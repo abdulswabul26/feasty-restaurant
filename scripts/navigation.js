6e9b4f8c9fcc385b158688b48175609e45c4d213
abdulswabul26 <abdulswabul26@gmail.com>
2024-09-03 11:56:53 +0300

    modified

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -5,13 +5,13 @@ document.addEventListener("DOMContentLoaded", function() {
   
     // Function to handle scroll event
     function handleScroll() {
-      if (window.scrollY > 120) {
+      if (window.scrollY > 600) {
         // If the scroll position is greater than 100, make the navbar transparent
-        navbar.style.backgroundColor = "transparent";
+        navbar.style.background= "linear-gradient(to left, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, .5))";
         navbar.style.backdropFilter = 'blur(10px) contrast(1.2)';
       } else {
         // Otherwise, reset the background color
-        navbar.style.backgroundColor = "transparent";  // You can set this to your default color
+        navbar.style.backgroundColor = "none";  // You can set this to your default color
       }
     }
   
