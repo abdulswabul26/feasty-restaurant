3aa8902e255ec314346a45d39ae29b795da42faf
abdulswabul26 <abdulswabul26@gmail.com>
2024-09-03 15:59:03 +0300

    modified

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,20 +1,22 @@
-// Wait for the DOM to load
+// NAvigation Scroll Effect
 document.addEventListener("DOMContentLoaded", function() {
     // Select the navigation bar
     const navbar = document.querySelector("nav");
   
     // Function to handle scroll event
     function handleScroll() {
-      if (window.scrollY > 600) {
-        // If the scroll position is greater than 100, make the navbar transparent
-        navbar.style.background= "linear-gradient(to left, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, .5))";
+      if (window.scrollY > 500) {
+        // If the scroll position is greater than 600, make the navbar transparent
+        navbar.style.background = "linear-gradient(to left, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, .5))";
         navbar.style.backdropFilter = 'blur(10px) contrast(1.2)';
       } else {
-        // Otherwise, reset the background color
-        navbar.style.backgroundColor = "none";  // You can set this to your default color
+        // Otherwise, reset the background to transparent
+        navbar.style.background = "transparent";
+        navbar.style.backdropFilter = 'none';
       }
     }
   
     // Attach the scroll event listener
     window.addEventListener("scroll", handleScroll);
-  });
+});
+
